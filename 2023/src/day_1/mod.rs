c1959f8015c7cc4be8f5dc96f235bfbf6e8f522f
Nick Edwards <46484451+nickjedwards@users.noreply.github.com>
2023-12-13 13:53:36 +1100

    feat: day 1 problem 2

--- 2023/src/day_1/mod.rs
+++ 2023/src/day_1/mod.rs
@@ -1,5 +1,6 @@
 pub fn solve(contents: &String) {
-    let lines = contents.lines();
+    // Replace spelled numbers with digits.
+    let lines: Vec<String> = contents.lines().map(replace_words_with_digit).collect();
 
     let mut calibrations: Vec<u32> = Vec::new();
 
@@ -22,4 +23,38 @@ pub fn solve(contents: &String) {
 
     // Sum those bad boys.
     println!("Day 1: {}", calibrations.iter().sum::<u32>());
+}
+
+fn replace_words_with_digit(line: &str) -> String {
+    let words = vec![
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ];
+
+    let mut matches: Vec<(usize, &str)> = Vec::new();
+
+    for (word, _digit) in &words {
+        for (k, v) in line.match_indices(word) {
+            matches.push((k, v));
+        }
+    }
+
+    matches.sort();
+
+    let mut mutated_string = String::from(line);
+
+    for (_k, v) in &matches {
+        if let Some(word) = words.iter().find(|(value, _)| value == v) {
+            mutated_string = mutated_string.replace(word.0, &[word.1, word.0].join(""));
+        }
+    }
+
+    mutated_string
 }
